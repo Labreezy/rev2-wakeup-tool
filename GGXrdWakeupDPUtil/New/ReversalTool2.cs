70babe410c6068c6c6d91a5d589f56d35dce21ab
Iquis <iquis01@gmail.com>
2018-04-16 16:54:02 +0200

    Get Dummy Loop

---
+++ GGXrdWakeupDPUtil/New/ReversalTool2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using Binarysharp.MemoryManagement;
+
+namespace GGXrdWakeupDPUtil
+{
+    public class ReversalTool2
+    {
+        private readonly string _ggprocname = ConfigurationManager.AppSettings.Get("GGProcessName");
+
+
+        private readonly List<NameWakeupData> _nameWakeupDataList = new List<NameWakeupData>
+        {
+            new NameWakeupData("Sol", 25, 21),
+            new NameWakeupData("Ky", 23, 21),
+            new NameWakeupData("May", 25, 22),
+            new NameWakeupData("Millia", 25, 23),
+            new NameWakeupData("Zato", 25, 22),
+            new NameWakeupData("Potemkin", 24, 22),
+            new NameWakeupData("Chipp", 30, 24),
+            new NameWakeupData("Faust", 25, 29),
+            new NameWakeupData("Axl", 25, 21),
+            new NameWakeupData("Venom", 21, 26),
+            new NameWakeupData("Slayer", 26, 20),
+            new NameWakeupData("I-No", 24, 20),
+            new NameWakeupData("Bedman", 24, 30),
+            new NameWakeupData("Ramlethal", 25, 23),
+            new NameWakeupData("Sin", 30, 21),
+            new NameWakeupData("Elphelt", 27, 27),
+            new NameWakeupData("Leo", 28, 26),
+            new NameWakeupData("Johnny", 25, 24),
+            new NameWakeupData("Jack-O'", 25, 23),
+            new NameWakeupData("Jam", 26, 25),
+            new NameWakeupData("Haehyun", 25, 27),
+            new NameWakeupData("Raven", 25, 24),
+            new NameWakeupData("Dizzy", 25, 24),
+            new NameWakeupData("Baiken", 22, 21),
+            new NameWakeupData("Answer", 24, 24)
+        };
+
+
+        #region Offsets
+        private readonly int _p2IdOffset = Convert.ToInt32(ConfigurationManager.AppSettings.Get("P2IdOffset"), 16);
+        #endregion
+
+
+        private MemorySharp _memorySharp;
+
+
+
+        public void AttachToProcess()
+        {
+            var process = Process.GetProcessesByName(_ggprocname).FirstOrDefault();
+
+            if (process == null)
+            {
+                throw new Exception("GG process not found!");
+            }
+
+            _memorySharp = new MemorySharp(process);
+        }
+
+        public NameWakeupData GetDummy()
+        {
+            var index = _memorySharp.Read<byte>((IntPtr)_p2IdOffset);
+
+            var result = _nameWakeupDataList[index];
+
+            return result;
+        }
+    }
+}
---
+++ GGXrdWakeupDPUtil/Window1.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GGXrdWakeupDPUtil
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private ReversalTool2 reversalTool;
+
+        private CancellationTokenSource dummyTokenSource = new CancellationTokenSource();
+        private CancellationToken dummyToken;
+        private NameWakeupData currentDummy;
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                reversalTool = new ReversalTool2();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Config invalid{Environment.NewLine}{ex.Message}");
+
+                Application.Current.Shutdown();
+                return;
+            }
+
+            try
+            {
+                reversalTool.AttachToProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Process not found!{Environment.NewLine}{ex.Message}");
+                Application.Current.Shutdown();
+                return;
+            }
+
+            StartDummyLoop();
+        }
+
+
+
+
+
+        private void StartDummyLoop()
+        {
+            dummyToken = dummyTokenSource.Token;
+
+            Task.Run(() =>
+            {
+                while (!dummyToken.IsCancellationRequested)
+                {
+                    var dummy = reversalTool.GetDummy();
+
+                    if (!Equals(dummy, currentDummy))
+                    {
+                        SetUIDummy(dummy);
+
+                        currentDummy = dummy;
+                    }
+                    Thread.Sleep(1000);
+                }
+
+            }, dummyToken);
+        }
+
+
+
+
+        //TODO Refactor with MVVM
+        #region UI
+        private void SetUIDummy(NameWakeupData dummy)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                dummyTextBlock.Text = $"Current Dummy :{dummy.CharName}";
+            });
+        }
+        #endregion
+    }
+}
