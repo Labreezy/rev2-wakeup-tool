f1ce5627503b62b9fa1776033a95aa141d651850
Iquis <iquis01@gmail.com>
2023-02-06 19:02:16 +0100

    feat combo event

--- GGXrdReversalTool.Library/Scenarios/Event/Implementations/ComboEvent.cs
+++ GGXrdReversalTool.Library/Scenarios/Event/Implementations/ComboEvent.cs
@@ -4,30 +4,26 @@ namespace GGXrdReversalTool.Library.Scenarios.Event.Implementations;
 
 public class ComboEvent : IScenarioEvent
 {
-    public int ComboCount
-    {
-        get => _comboCount;
-        set => _comboCount = value;
-    }
+    public int MinComboCount { get; set; } = 1;
+    public int MaxComboCount { get; set; } = 5;
 
     public IMemoryReader MemoryReader { get; set; }
 
     private int _oldComboCount;
-    private int _comboCount = 5;
 
     public ScenarioEventTypes CheckEvent()
     {
         var comboCount = MemoryReader.GetComboCount(1);
 
-        //TODO Implement parameter for combo values
-        if (comboCount == ComboCount && _oldComboCount != comboCount)
+        if (comboCount >= MinComboCount && comboCount<= MaxComboCount && _oldComboCount != comboCount)
         {
             _oldComboCount = comboCount;
             return ScenarioEventTypes.Combo;
         }
-        
+
         _oldComboCount = comboCount;
 
         return ScenarioEventTypes.None;
     }
+
 }
